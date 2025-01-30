f06f90e7479e063d03174318ba8dd9b9937f22b9
Li Fang <145475982+Challengecod@users.noreply.github.com>
2025-01-29 21:28:54 -0500

    Part 1 checked for the right count amount and assigned it to the right type

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,36 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         // step 1: create a file object
-        File f = new File("C:\\Users\\BT_2S14_02\\IdeaProjects\\Unit 6 Project\\src\\Input");
+        File f = new File("/Users/andyfang/IdeaProjects/unit 6/src/input");
 
         String fileData = "";
+        String bidData = "";
+        HashMap<String, Integer> cardInformation = new HashMap<>();
         try {
 
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
                 String currentLine = s.nextLine();
-
                 int verticalLine = currentLine.indexOf("|");
+                String bidLine = currentLine.substring(verticalLine+1);
+                bidData += bidLine + "\n";
+                int bidLineInt = Integer.parseInt(bidLine);
                 currentLine = currentLine.substring(0, verticalLine);
                 fileData += currentLine + "\n";
-
-
+                cardInformation.put(currentLine, bidLineInt);
             }
 
+            System.out.println(cardInformation);
+
+
+            String[] bidArray = bidData.split("\n"); // bid values array
+            System.out.println(Arrays.toString(bidArray));
             // a String array where every item in the array is a line from the file
             String[] fileArray = fileData.split("\n");
             System.out.println(Arrays.toString(fileArray));
@@ -35,10 +42,17 @@ public class Main {
             int fiveKind = 0;
             int highCard = 0;
 
+            ArrayList<String> highCardList = new ArrayList();
+            ArrayList<String> onePairList = new ArrayList();
+            ArrayList<String> twoPairList = new ArrayList();
+            ArrayList<String> threeKindList = new ArrayList();
+            ArrayList<String> fullHouseList = new ArrayList();
+            ArrayList<String> fourKindList = new ArrayList();
+            ArrayList<String> fiveKindList = new ArrayList();
+
             for (String line : fileArray) {
                 System.out.println(line);
                 String[] lineList = line.split(",");
-                System.out.println(Arrays.toString(lineList));
                 int count = 0;
                 for(int i = 0; i < lineList.length; i++){
                     for(int j = i+1; j < lineList.length; j++){
@@ -53,18 +67,61 @@ public class Main {
 
                 if (count == 1) {
                     onePair += 1;
+                    onePairList.add(line);
+                    System.out.println("One Pair");
 
                 }
                 if (count == 2) {
                     twoPair += 1;
+                    twoPairList.add(line);
+                    System.out.println("Two Pair");
 
                 }
                 if(count == 3){
                     threeKind += 1;
+                    threeKindList.add(line);
+                    System.out.println("Three Kind");
                 }
 
+                if(count == 0){
+                    highCard += 1;
+                    highCardList.add(line);
+                    System.out.println("High card");
+                }
 
+                if(count == 4){
+                    fullHouse += 1;
+                    fullHouseList.add(line);
+                    System.out.println("Full House");
+                }
+
+                if(count == 6){
+                    fourOfKind += 1;
+                    fourKindList.add(line);
+                    System.out.println("Four Kind ");
+                }
+
+                if(count == 10){
+                    fiveKind += 1;
+                    fiveKindList.add(line);
+                    System.out.println("Five Kind ");
+                }
             }
+            System.out.println(onePairList);
+            System.out.println(twoPairList);
+            System.out.println(threeKindList);
+            onePairList = order(onePairList);
+            twoPairList = order(twoPairList);
+
+            // creating new empty list
+            // List Concatenation;
+            List<String> concatenated_list = new ArrayList<>();
+            concatenated_list.addAll(onePairList);
+            concatenated_list.addAll(twoPairList);
+            System.out.println(concatenated_list);
+
+            // get the bid value
+            System.out.println(cardInformation.get(concatenated_list.get(0)));
 
             System.out.println(
                     "Number of five of a kind hands: "+ fiveKind + "\n"  +
@@ -76,9 +133,6 @@ public class Main {
                             "Number of high card hands "+ highCard
             );
 
-
-
-
         }
         catch (FileNotFoundException fe) {
             System.out.println("File was not found");
@@ -87,50 +141,17 @@ public class Main {
 
     }
 
-    public static int checkCard(String value){
-        if(value.equals("Ace")){
-            return 0;
-        }
-        if(value.equals("King")){
-            return 1;
-        }
-        if(value.equals("Queen")){
-            return 2;
-        }
-        if(value.equals("Jack")){
-            return 3;
-        }
-        if(value.equals("10")){
-            return 4;
-        }
-        if(value.equals("9")){
-            return 5;
-        }
-        if(value.equals("8")){
-            return 6;
-        }
-        if(value.equals("7")){
-            return 7;
-        }
-        if(value.equals("6")){
-            return 8;
-        }
-        if(value.equals("5")){
-            return 9;
-        }
-        if(value.equals("4")){
-            return 10;
-        }
-        if(value.equals("3")){
-            return 11;
-        }
-        if(value.equals("2")){
-            return 12;
-        }
-        else{
-            return 13;
+    public static ArrayList<String> order(ArrayList<String> line){
+        ArrayList<String> ordered = new ArrayList<>();
+        if(line.size() <= 1){
+            return line;
         }
 
+        else {
+            ordered.add("King,Ace,Ace,Ace,Ace");
+        }
+        return ordered;
     }
+
 
 }
